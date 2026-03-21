ed5c415a22daab646245b947012af081abb82cf4
gaosai <995713480@qq.com>
2026-03-21 23:48:00 +0800

    🔧 fix(config): vite代理端口改为8000，补充.gitignore环境变量规则

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/auth': 'http://127.0.0.1:8080',
-      '/danmu': 'http://127.0.0.1:8080',
-      '/api': 'http://127.0.0.1:8080',
-      '/photos': 'http://127.0.0.1:8080',
-      '/photos.json': 'http://127.0.0.1:8080',
+      '/auth': 'http://127.0.0.1:8000',
+      '/danmu': 'http://127.0.0.1:8000',
+      '/api': 'http://127.0.0.1:8000',
+      '/photos': 'http://127.0.0.1:8000',
+      '/photos.json': 'http://127.0.0.1:8000',
     }
   },
   build: {
