f03501d626958ff9aea10e0e24e04eceae0b9364
gaosai <995713480@qq.com>
2026-06-17 17:39:59 +0800

    旅游

--- outputs/excel_edit_20260615/help_number_format.mjs
+++
@@ -1,5 +0,0 @@
-import { FileBlob, SpreadsheetFile } from "@oai/artifact-tool";
-const input = await FileBlob.load(process.argv[2]);
-const workbook = await SpreadsheetFile.importXlsx(input);
-const help = workbook.help("range.numberFormat", { include: "examples,notes", maxChars: 2500 });
-console.log(help.ndjson);
--- outputs/excel_edit_20260615/help_number_format2.mjs
+++
@@ -1,5 +0,0 @@
-import { FileBlob, SpreadsheetFile } from "@oai/artifact-tool";
-const input = await FileBlob.load(process.argv[2]);
-const workbook = await SpreadsheetFile.importXlsx(input);
-const help = workbook.help("numberFormat", { include: "index,examples,notes", maxChars: 3000 });
-console.log(help.ndjson);
